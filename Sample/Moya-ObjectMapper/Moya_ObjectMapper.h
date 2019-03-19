00fa95c85c8d7857383d9e39922f0e6d7f3de647
kceejp <hi@kcee.jp>
2019-03-20 05:10:39 +0900

    Add shared schemes to fix carthage support

---
+++ Sample/Moya-ObjectMapper/Moya_ObjectMapper.h
@@ -0,0 +1,19 @@
+//
+//  Moya_ObjectMapper.h
+//  Moya-ObjectMapper
+//
+//  Created by Keishi Maeda on 2019/03/20.
+//  Copyright © 2019 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Moya_ObjectMapper.
+FOUNDATION_EXPORT double Moya_ObjectMapperVersionNumber;
+
+//! Project version string for Moya_ObjectMapper.
+FOUNDATION_EXPORT const unsigned char Moya_ObjectMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moya_ObjectMapper/PublicHeader.h>
+
+
